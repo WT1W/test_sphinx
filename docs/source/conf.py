406069a9b0740099842f0dd718bd3043b154e827
WT1W <757539294@qq.com>
2025-07-21 11:43:35 +0000

    change doc name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,6 +87,9 @@ html_theme = 'pydata_sphinx_theme'
 html_baseurl = "https://wt1w.github.io/test_sphinx/blog8/"
 version = "5.2"
 html_theme_options = {
+    "logo": {
+        "text": "MagiAttention",
+    },
     "navbar_start": ["navbar-logo", "version-switcher"],
     "switcher": {
         "json_url": "https://raw.githubusercontent.com/WT1W/test_sphinx/refs/heads/main/work.json",
