2511bb474ed8cd4a6f764ba611de9b44b1664fc0
WT1W <757539294@qq.com>
2025-07-21 11:55:11 +0000

    add png

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,8 +88,8 @@ html_baseurl = "https://wt1w.github.io/test_sphinx/blog8/"
 version = "5.2"
 html_theme_options = {
     "logo": {
-        "image_light": "_static/sand-logos/magi-icon.png",
-        "image_dark": "_static/sand-logos/magi-icon.png",
+        "image_light": "_static/sand-logos/sand-log-square.png",
+        "image_dark": "_static/sand-logos/sand-log-square.png",
         "text": "MagiAttention",
     },
     "navbar_start": ["navbar-logo", "version-switcher"],
